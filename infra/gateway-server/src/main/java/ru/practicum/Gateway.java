9b3d165f225c8c3dcac342180b0f3f2ae0798442
DmtritPlesko <dima.pleshko.03@mail.ru>
2025-02-20 18:28:57 +0400

    Frist commit

--- infra/gateway-server/src/main/java/ru/practicum/Gateway.java
+++ infra/gateway-server/src/main/java/ru/practicum/Gateway.java
@@ -1,14 +1,13 @@
-package ewm;
+package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
 @SpringBootApplication
 @EnableDiscoveryClient
-public class MainApplication {
-    public static void main(String[] args) throws InterruptedException {
-        SpringApplication.run(MainApplication.class, args);
+public class Gateway {
+    public static void main(String[] args) {
+        SpringApplication.run(Gateway.class,args);
     }
 }
